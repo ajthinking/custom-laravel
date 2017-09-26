e38b47a64c63a758e61c04b680c5f2baca94a763
Anders Jürisoo <jurisoo@hotmail.com>
2017-09-26 21:24:03 +0200

    Initial commit

---
+++ custom-laravel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+laravel new $1
+cd $1
+echo $PWD
+touch storage/database.sqlite
+sed -i '8s/.*/DB_CONNECTION=sqlite/' .env
+sed -i "9s/.*/DB_DATABASE=\/home\/anders\/Code\/$1\/storage\/database.sqlite/" .env
+sed -i '10s/.*//' .env
+sed -i '11s/.*//' .env
+sed -i '12s/.*//' .env
+sed -i '13s/.*//' .env
+
+php artisan make:auth
+php artisan migrate:fresh
