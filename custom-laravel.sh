8e4139235ee6182dd2a1a8ce827d4323b552c801
Anders Jürisoo <jurisoo@hotmail.com>
2017-09-28 08:20:50 +0200

    Update custom-laravel.sh

--- custom-laravel.sh
+++ custom-laravel.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+
 laravel new $1
 cd $1
-echo $PWD
+
+composer require guzzlehttp/guzzle
+composer require --dev ajthinking/modeltables
+composer require laravelcollective/html
+
 touch storage/database.sqlite
+
 sed -i '8s/.*/DB_CONNECTION=sqlite/' .env
 sed -i "9s/.*/DB_DATABASE=\/home\/anders\/Code\/$1\/storage\/database.sqlite/" .env
 sed -i '10s/.*//' .env
 sed -i '11s/.*//' .env
 sed -i '12s/.*//' .env
 sed -i '13s/.*//' .env
-
-composer require --dev ajthinking/modeltables
-composer require laravelcollective/html
-
 
 php artisan make:auth
 php artisan migrate:fresh
